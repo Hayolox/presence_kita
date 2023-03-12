94bba24b6abafc553ebef502c6a863b235e7e024
hayolox <akiklii72@gmail.com>
2023-03-12 20:49:54 +0700

    featureHome

--- lib/screen/student/home/home_page.dart
+++ lib/screen/student/home/home_page.dart
@@ -90,7 +90,7 @@ class _HomePageState extends State<HomePage> {
                           ),
                         ),
                         Text(
-                          '193010503011',
+                          value.student[2],
                           style: primaryTextStyle.copyWith(
                             fontSize: 13,
                           ),
@@ -109,80 +109,87 @@ class _HomePageState extends State<HomePage> {
                           height: 22,
                         ),
                         Expanded(
-                          child: ListView.builder(
-                            itemCount: value.dataHome.subject.length,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: const EdgeInsets.only(
-                                  bottom: 10,
-                                ),
-                                child: GestureDetector(
-                                  onTap: () => Navigator.pushNamed(
-                                      context, '/sessionPage'),
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 15,
-                                    ),
-                                    height: 77,
-                                    decoration: BoxDecoration(
-                                      border: Border.all(width: 1),
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceAround,
-                                      children: [
-                                        Container(
-                                          height: 45,
-                                          width: 45,
-                                          decoration: BoxDecoration(
-                                            color: const Color(0xffAEC8E7),
-                                            borderRadius:
-                                                BorderRadius.circular(25),
-                                          ),
-                                          child: Center(
-                                            child: Text(
-                                              value.dataHome.subject[index]
-                                                  .subject.nickname
-                                                  .substring(0, 1),
-                                              style: primaryTextStyle.copyWith(
-                                                color: Colors.white,
-                                                fontSize: 22,
+                          child: RefreshIndicator(
+                            onRefresh: () => value.getDataHome(),
+                            child: ListView.builder(
+                              itemCount: value.dataHome.subject.length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.only(
+                                    bottom: 10,
+                                  ),
+                                  child: GestureDetector(
+                                    onTap: () => Navigator.pushNamed(
+                                        context, '/sessionPage'),
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: 15,
+                                      ),
+                                      height: 77,
+                                      decoration: BoxDecoration(
+                                        border: Border.all(width: 1),
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Container(
+                                            height: 45,
+                                            width: 45,
+                                            decoration: BoxDecoration(
+                                              color: const Color(0xffAEC8E7),
+                                              borderRadius:
+                                                  BorderRadius.circular(25),
+                                            ),
+                                            child: Center(
+                                              child: Text(
+                                                value.dataHome.subject[index]
+                                                    .subject.nickname
+                                                    .substring(0, 1),
+                                                style:
+                                                    primaryTextStyle.copyWith(
+                                                  color: Colors.white,
+                                                  fontSize: 22,
+                                                ),
                                               ),
                                             ),
                                           ),
-                                        ),
-                                        Text(
-                                          value.dataHome.subject[index].subject
-                                              .nickname,
-                                          style: primaryTextStyle.copyWith(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold,
+                                          Text(
+                                            value.dataHome.subject[index]
+                                                .subject.nickname,
+                                            style: primaryTextStyle.copyWith(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                            ),
                                           ),
-                                        ),
-                                        Row(
-                                          children: [
-                                            Text(
-                                              value.dataHome
-                                                  .countStudentInSubject[index]
-                                                  .toString(),
-                                              style: primaryTextStyle.copyWith(
-                                                fontSize: 16,
-                                                fontWeight: FontWeight.bold,
+                                          Row(
+                                            children: [
+                                              Text(
+                                                value
+                                                    .dataHome
+                                                    .countStudentInSubject[
+                                                        index]
+                                                    .toString(),
+                                                style:
+                                                    primaryTextStyle.copyWith(
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
                                               ),
-                                            ),
-                                            const SizedBox(
-                                              width: 6,
-                                            ),
-                                            const Icon(Icons.people)
-                                          ],
-                                        )
-                                      ],
+                                              const SizedBox(
+                                                width: 6,
+                                              ),
+                                              const Icon(Icons.people)
+                                            ],
+                                          )
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                              );
-                            },
+                                );
+                              },
+                            ),
                           ),
                         )
                       ],
